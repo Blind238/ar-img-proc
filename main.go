514bbb9a7c591ca2ea0d001eb1b70087e8ad9f7b
Jeremy Granadillo <akakitsu@gmail.com>
2016-12-22 11:51:06 +0100

    add yuv image

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"image"
 	"image/png"
 	"log"
@@ -47,12 +48,14 @@ func main() {
 
 	http.HandleFunc("/reference", refHandler)
 	http.HandleFunc("/grayscale", grayHandler)
+	http.HandleFunc("/yuv", yuvHandler)
 	http.HandleFunc("/", handler)
 
 	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println("ready and listening on :8080")
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -93,6 +96,96 @@ func grayHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func yuvHandler(w http.ResponseWriter, req *http.Request) {
+	// convert to YUV and back again (and be able to change Y value)
+
+	yuv := make([][]float32, len(ref.Pix)/4)
+	p := make([]uint8, 4)
+
+	for i := 0; i < len(ref.Pix); i++ {
+		p[i%4] = ref.Pix[i]
+
+		if i%4 == 3 {
+			y, u, v := rgbToYUV(p[0], p[1], p[2])
+
+			x := (i+1)/4 - 1
+
+			yuv[x] = make([]float32, 3)
+
+			yuv[x][0] = y
+			yuv[x][1] = u
+			yuv[x][2] = v
+
+		}
+	}
+
+	log.Println(yuv)
+
+	rgb := image.NewNRGBA(ref.Bounds())
+
+	for i := 0; i < len(yuv); i++ {
+		r, g, b := yuvToRGB(yuv[i][0], yuv[i][1], yuv[i][2])
+		rgb.Pix[i*4] = r
+		rgb.Pix[i*4+1] = g
+		rgb.Pix[i*4+2] = b
+		rgb.Pix[i*4+3] = 255 //alpha
+	}
+
+	err := writePng(w, rgb)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func rgbToYUV(r uint8, g uint8, b uint8) (y float32, u float32, v float32) {
+	// define constants
+	var rconst float32 = 0.299
+	var gconst float32 = 0.587
+	var bconst float32 = 0.114
+	var uMax float32 = 0.436
+	var vMax float32 = 0.615
+
+	y = rconst*float32(r) + gconst*float32(g) + bconst*float32(b)
+	fmt.Println(y)
+	if y > 1 {
+		y = 1
+	}
+	if y < -1 {
+		y = -1
+	}
+
+	u = 0.492 * (float32(b) - y)
+	v = 0.877 * (float32(r) - y)
+	fmt.Println(u, v)
+
+	if u > uMax {
+		u = uMax
+	}
+	if u < -uMax {
+		u = -uMax
+	}
+
+	if v > vMax {
+		v = vMax
+	}
+	if v < -vMax {
+		v = -vMax
+	}
+
+	fmt.Println(r, g, b, y, u, v)
+
+	return y, u, v
+}
+
+func yuvToRGB(y float32, u float32, v float32) (r uint8, g uint8, b uint8) {
+
+	r = uint8(y + 1.14*v)
+	g = uint8(y - 0.395*u - 0.581*v)
+	b = uint8(y + 2.033*u)
+
+	return r, g, b
 }
 
 func writePng(w http.ResponseWriter, m image.Image) error {
