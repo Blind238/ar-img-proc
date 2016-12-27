eb8b4389cc57c0225c27ae48ea3029f45a6375a0
Jeremy Granadillo <akakitsu@gmail.com>
2016-12-27 22:33:30 +0100

    m -> n -> ref ==> m -> ref

--- main.go
+++ main.go
@@ -24,24 +24,7 @@ func main() {
 	}
 	f.Close()
 
-	//nData := myImage(mData) => cannot convert mData (type image.Image) to
-	//                           type myImage
-	n := m.(*image.NRGBA)
-
-	ref = n
-
-	/*fmt.Println("read and decoded file, printing colors")
-	//p := []uint8{} // for R,G,B,A
-	p := make([]uint8, 4) // for R,G,B,A
-
-	for i := 0; i < len(n.Pix); i++ {
-		p[i%4] = n.Pix[i]
-		//fmt.Println(n.Pix[i])
-
-		if i%4 == 3 {
-			// fmt.Println(p)
-		}
-	}*/
+	ref = m.(*image.NRGBA)
 
 	http.HandleFunc("/reference", refHandler)
 	http.HandleFunc("/grayscale", grayHandler)
